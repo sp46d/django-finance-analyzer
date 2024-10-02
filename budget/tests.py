958814ef90537d58c5f650e6691129fa34a4923a
Sanghyuk Park <sanghyuk.park85@gmail.com>
2024-10-02 13:53:54 -0500

    Create a view function that renders home.html and corresponding test

--- budget/tests.py
+++ budget/tests.py
@@ -1,5 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+# This file is for unit test. From my understanding, two processes need to get tested:
+# 1. if the veiw function uses the correct template
+# 2. if the view function works as intended
+
+
 class HomePageTest(TestCase):
-    
+    def test_uses_home_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "budget/home.html")
