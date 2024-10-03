8ffe67c4c8790861cb84ab8c00414bf9f25b75b8
Sanghyuk Park <sanghyuk.park85@gmail.com>
2024-10-02 23:33:25 -0500

    Create Transaction data model

--- budget/models.py
+++ budget/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class Transaction(models.Model):
+    TRANSACTION_TYPE_CHOICES = [
+        ("IN", "Income"),
+        ("EX", "Expense"),
+        ("TR", "Transfer"),
+    ]
+    
+    date=models.DateField(default=date.today)
+    transaction_type=models.CharField(
+        max_length=3,
+        choices=TRANSACTION_TYPE_CHOICES,
+        default="IN"
+    )
+    description=models.TextField()
+    amount=models.DecimalField(max_digits=10, decimal_places=2)
